b4ee4d9098bbd9824c446e1a8bd91ef158f7d0c6
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2024-11-08 20:46:23 -0600

    research_env demo

--- marble/environments/__init__.py
+++ marble/environments/__init__.py
@@ -1,7 +1,9 @@
 from .base_env import BaseEnvironment
+from .research_env import ResearchEnvironment
 from .web_env import WebEnvironment
 
 __all__ = [
     'BaseEnvironment',
     'WebEnvironment',
+    'ResearchEnvironment'
 ]
