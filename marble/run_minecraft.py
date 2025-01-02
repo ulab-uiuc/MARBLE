6c91860340c85bf7a38f97fd34ebf6bfae5918b6
yxc-cyber <yxc_yxc@yeah.net>
2025-01-02 21:02:54 +0800

    10 test cases updated

--- marble/run_minecraft.py
+++ marble/run_minecraft.py
@@ -10,7 +10,7 @@ import os
 
 if __name__ == "__main__":
     os.chdir("marble")
-    for model in ["gpt-4o-mini"]:
+    for model in ["gpt-35-turbo"]:
         for task_id in range(10):
             config = f"configs/test_config_minecraft/test_config_{model}_{task_id}.yaml"
             os.system(f"python main.py --config_path {config}")
